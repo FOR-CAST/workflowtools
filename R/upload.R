2b07215c39540002cefd5fcfa134dad1d84f3601
Alex Chubaty <achubaty@for-cast.ca>
2023-09-28 11:22:46 -0600

    add imports for upload.R

--- R/upload.R
+++ R/upload.R
@@ -22,38 +22,43 @@ utils::globalVariables(c(
 #' @return A dribble of the uploaded files (not directories though.)
 #'
 #' @export
+#' @importFrom dplyr bind_rows filter pull
+#' @importFrom fs dir_info
+#' @importFrom furrr future_map_dfr future_map2_dfr
+#' @importFrom googledrive drive_ls drive_mkdir drive_put
+#' @importFrom httr config set_config
 drive_upload_folder <- function(folder, drive_path, batch_size = 10) {
   ## avoid curl HTTP2 framing layer error:
   ## per https://github.com/tidyverse/googlesheets4/issues/233#issuecomment-889376499
-  old <- httr::set_config(httr::config(http_version = 2)) ## corresponds to CURL_HTTP_VERSION_1_1
-  on.exit(httr::set_config(old), add = TRUE)
+  old <- set_config(config(http_version = 2)) ## corresponds to CURL_HTTP_VERSION_1_1
+  on.exit(set_config(old), add = TRUE)
 
   ## Only call fs::dir_info once in order to avoid weirdness if the contents of the folder is changing
-  contents <- fs::dir_info(folder, type = c("file", "dir"))
+  contents <- dir_info(folder, type = c("file", "dir"))
   dirs_to_upload <- contents |>
     dplyr::filter(type == "directory") |>
-    dplyr::pull(path)
+    pull(path)
 
-  folderIDs <- googledrive::drive_ls(drive_path)
+  folderIDs <- drive_ls(drive_path)
   fid <- folderIDs[folderIDs[["name"]] == basename(folder), "id"][[1]]
   if (length(fid) == 0) {
-    fid <- googledrive::drive_mkdir(basename(folder), drive_path)[["id"]]
+    fid <- drive_mkdir(basename(folder), drive_path)[["id"]]
   }
 
   # Directly upload the files
   files_to_upload <- contents |>
     dplyr::filter(type == "file") |>
-    dplyr::pull(path)
+    pull(path)
   g <- seq(files_to_upload) %/% batch_size
   uploaded_files <- files_to_upload |>
     split(g) |>
     lapply(function(x) {
       future_map_dfr(x, googledrive::drive_put, path = fid)
     }) |>
-    dplyr::bind_rows()
+    bind_rows()
 
   # Create the next level down of directories
-  furrr::future_map2_dfr(dirs_to_upload, fid, drive_upload_folder) |>
-    dplyr::bind_rows(uploaded_files) |>
+  future_map2_dfr(dirs_to_upload, fid, drive_upload_folder) |>
+    bind_rows(uploaded_files) |>
     invisible() ## return a dribble of what's been uploaded
 }
