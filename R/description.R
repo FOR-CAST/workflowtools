81df98ff1bdd33b7f0f40a4a955f28e696900971
Alex Chubaty <achubaty@for-cast.ca>
2024-03-04 11:50:28 -0700

    allow user to pass existing pkg snapshot to description()

--- R/description.R
+++ R/description.R
@@ -56,42 +56,48 @@ description <- function(fields = list(), library = NULL, snapshot = NULL) {
     library <- .libPaths()
   }
 
+  useRequire <- isTRUE(getOption("workflowtools.useRequire"))
+
   if (is.null(snapshot)) {
-    if (isTRUE(getOption("workflowtools.useRequire"))) {
-      if (requireNamespace("Require", quietly = TRUE)) {
-        snapshot <- tempfile("pkgsnapshot_Require_", fileext = ".csv")
-        Require::pkgSnapshot(packageVersionFile = snapshot, libPaths = library)
-        on.exit(unlink(snapshot), add = TRUE)
-
-        pkgs <- read.csv(snapshot)
-        colnames(pkgs) <- colnames(pkgs) |>
-          gsub("Github", "Remote", x = _) |>
-          gsub("SHA1", "Sha", x = _)
-        pkgs <- as.data.table(pkgs)
-      } else {
-        stop("Suggested package 'Require' is not installed.")
-      }
+    if (useRequire) {
+      snapshot <- tempfile("pkgsnapshot_Require_", fileext = ".csv")
     } else {
-      if (requireNamespace("renv", quietly = TRUE) &&
-          requireNamespace("jsonlite", quietly = TRUE)) {
-        snapshot <- tempfile("pkgsnapshot_renv_", fileext = ".lock")
-        renv::snapshot(library = library, lockfile = snapshot, type = "all")
-        on.exit(unlink(snapshot), add = TRUE)
-
-        pkgs <- jsonlite::fromJSON(txt = "renv.lock")[["Packages"]] |>
-          lapply(as.data.frame) |>
-          rbindlist(fill = TRUE)
-        set(pkgs, NULL, "Requirements", NULL)
-      } else {
-        stop("Suggested packages 'renv' or 'jsonlite' are not installed.")
-      }
+      snapshot <- tempfile("pkgsnapshot_renv_", fileext = ".lock")
     }
 
-    pkgs <- pkgs[!duplicated(pkgs)]
-
-    cranPkgs <- pkgs[is.na(RemoteUsername), ]
-    ghPkgs <- pkgs[!is.na(RemoteUsername), ]
+    on.exit(unlink(snapshot), add = TRUE)
   }
+
+  if (useRequire) {
+    if (requireNamespace("Require", quietly = TRUE)) {
+      Require::pkgSnapshot(packageVersionFile = snapshot, libPaths = library)
+
+      pkgs <- read.csv(snapshot)
+      colnames(pkgs) <- colnames(pkgs) |>
+        gsub("Github", "Remote", x = _) |>
+        gsub("SHA1", "Sha", x = _)
+      pkgs <- as.data.table(pkgs)
+    } else {
+      stop("Suggested package 'Require' is not installed.")
+    }
+  } else {
+    if (requireNamespace("renv", quietly = TRUE) &&
+        requireNamespace("jsonlite", quietly = TRUE)) {
+      renv::snapshot(library = library, lockfile = snapshot, type = "all")
+
+      pkgs <- jsonlite::fromJSON(txt = "renv.lock")[["Packages"]] |>
+        lapply(as.data.frame) |>
+        rbindlist(fill = TRUE)
+      set(pkgs, NULL, "Requirements", NULL)
+    } else {
+      stop("Suggested packages 'renv' or 'jsonlite' are not installed.")
+    }
+  }
+
+  pkgs <- pkgs[!duplicated(pkgs)]
+
+  cranPkgs <- pkgs[is.na(RemoteUsername), ]
+  ghPkgs <- pkgs[!is.na(RemoteUsername), ]
 
   ## override user-specified field values
   fields <- modList(fields, list(
